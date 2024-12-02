e3fcfa57ccc592d437e4e52c3d4b2534711593a8
Róger Santos <34362175+BornSaint@users.noreply.github.com>
2024-12-02 14:21:48 -0300

    Update components.py

--- date_spacy/components.py
+++ date_spacy/components.py
@@ -2,21 +2,34 @@ import re
 from spacy.tokens import Span
 from spacy.language import Language
 from spacy.util import filter_spans
+
 import dateparser
+from collections import OrderedDict
 
 ordinal_to_number = {
         "primeiro": "1", "segundo": "2", "terceiro": "3", "quarto": "4", "quinto": "5",
-        "sexto": "6", "sétimo": "7", "oitavo": "8", "nono": "9", "décimo-": "10",
-        "décimo-primeiro": "11", "décimo-segundo": "12", "décimo-terceiro": "13", "décimo-quarto": "14",
-        "décimo-quinto": "15", "décimo-sexto": "16", "décimo-sétimo": "17", "décimo-oitavo": "18",
-        "décimo-nono": "19", "vigésimo": "20", "vigésimo-primeiro": "21", "vigésimo-segundo": "22",
-        "vigésimo-terceiro": "23", "vigésimo-quarto": "24", "vigésimo-quinto": "25", "vigésimo-sexto": "26",
-        "vigésimo-sétimo": "27", "vigésimo-oitavo": "28", "vigésimo-nono": "29", "trigésimo": "30",
-        "trigésimo-primeiro": "31"
+        "sexto": "6", "sétimo": "7", "oitavo": "8", "nono": "9", "décimo": "10",
+        "décimo[-\s]primeiro": "11", "décimo[-\s]segundo": "12", "décimo[-\s]terceiro": "13", "décimo[-\s]quarto": "14",
+        "décimo[-\s]quinto": "15", "décimo[-\s]sexto": "16", "décimo[-\s]sétimo": "17", "décimo[-\s]oitavo": "18",
+        "décimo[-\s]nono": "19", "vigésimo": "20", "vigésimo[-\s]primeiro": "21", "vigésimo[-\s]segundo": "22",
+        "vigésimo[-\s]terceiro": "23", "vigésimo[-\s]quarto": "24", "vigésimo[-\s]quinto": "25", "vigésimo[-\s]sexto": "26",
+        "vigésimo[-\s]sétimo": "27", "vigésimo[-\s]oitavo": "28", "vigésimo[-\s]nono": "29", "trigésimo": "30",
+        "trigésimo[-\s]primeiro": "31"
 
 }
 
+reversed_ordinal_to_number = OrderedDict(reversed(list(ordinal_to_number.items())))
+# ordinal_to_number = {
+#         "primeiro": "1", "segundo": "2", "terceiro": "3", "quarto": "4", "quinto": "5",
+#         "sexto": "6", "sétimo": "7", "oitavo": "8", "nono": "9", "décimo": "10",
+#         "décimo primeiro": "11", "décimo segundo": "12", "décimo terceiro": "13", "décimo quarto": "14",
+#         "décimo quinto": "15", "décimo sexto": "16", "décimo sétimo": "17", "décimo oitavo": "18",
+#         "décimo nono": "19", "vigésimo": "20", "vigésimo primeiro": "21", "vigésimo segundo": "22",
+#         "vigésimo terceiro": "23", "vigésimo quarto": "24", "vigésimo quinto": "25", "vigésimo sexto": "26",
+#         "vigésimo sétimo": "27", "vigésimo oitavo": "28", "vigésimo nono": "29", "trigésimo": "30",
+#         "trigésimo primeiro": "31"
 
+# }
 @Language.component("find_dates")
 def find_dates(doc):
     # Set up a date extension on the span
@@ -34,15 +47,15 @@ def find_dates(doc):
     #     "twenty-seventh", "twenty-eighth", "twenty-ninth", "thirtieth", "thirty-first"
     # ]
     
-    ordinal_pattern = r"\b(?:" + "|".join(ordinals) + r")\b"
+    ordinal_pattern = r"\b(?:" + "(\s*dia\s*)?|".join(ordinals) + r")\b"
 
     # A regex pattern to capture a variety of date formats
     date_pattern = r"""
         # Day-Month-Year
         (?:
-            \d{1,2}(?:st|nd|rd|th)?     # Day with optional st, nd, rd, th suffix
+            \d{1,2}(?:º)?     # Day with optional st, nd, rd, th suffix
             (\s+de\s+)?
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]* # Month name
+            (?:[Jj]an|[Ff]ev|[Mm]ar|[Aa]br|[Mm]ai|[Jj]un|[Jj]ul|[Aa]go|[Ss]et|[Oo]ut|[Nn]ov|[Dd]ez)[a-z]* # Month name
             (\s+de\s+)?
             (?:                         # Year is optional
                 \s*
@@ -71,25 +84,21 @@ def find_dates(doc):
         )
         |
         # Month-Day-Year
-        (\s+de\s+)?
         (?:
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]* # Month name
-            (\s+de\s+)?
-            \d{1,2}(?:st|nd|rd|th)?     # Day with optional st, nd, rd, th suffix
-            (\s+de\s+)?
+            (?:[Jj]an|[Ff]ev|[Mm]ar|[Aa]br|[Mm]ai|[Jj]un|[Jj]ul|[Aa]go|[Ss]et|[Oo]ut|[Nn]ov|[Dd]ez)[a-z]* # Month name
+            \d{1,2}(?:º)?     # Day with optional st, nd, rd, th suffix
+            
             (?:                         # Year is optional
                 ,?
-                \s*
+                \s+
                 \d{4}                   # Year
             )?
         )
         |
         # Month-Year
-        (\s+de\s+)?
         (?:
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]* # Month name
+            (?:[Jj]an|[Ff]ev|[Mm]ar|[Aa]br|[Mm]ai|[Jj]un|[Jj]ul|[Aa]go|[Ss]et|[Oo]ut|[Nn]ov|[Dd]ez)[a-z]* # Month name
             (\s+de\s+)?
-            \s*
             \d{4}                       # Year
         )
         |
@@ -97,10 +106,9 @@ def find_dates(doc):
         (?:
             """ + ordinal_pattern + """
             (\s+de\s+)?
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]* # Month name
+            (?:[Jj]an|[Ff]ev|[Mm]ar|[Aa]br|[Mm]ai|[Jj]un|[Jj]ul|[Aa]go|[Ss]et|[Oo]ut|[Nn]ov|[Dd]ez)[a-z]* # Month name
             (\s+de\s+)?
             (?:                         # Year is optional
-                \s*
                 \d{4}                   # Year
             )?
         )
@@ -108,30 +116,18 @@ def find_dates(doc):
         (?:
             """ + ordinal_pattern + """
             (\s+de\s+)?
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]*  # Month name
+            (?:[Jj]an|[Ff]ev|[Mm]ar|[Aa]br|[Mm]ai|[Jj]un|[Jj]ul|[Aa]go|[Ss]et|[Oo]ut|[Nn]ov|[Dd]ez)[a-z]*  # Month name
             (\s+de\s+)?
             (?:                         # Year is optional
-                \s*
                 \d{4}                   # Year
             )?
         )
-        |
-        # Month Ordinal
-        (\s+de\s+)?
-        (?:
-            (?:Jan|Fev|Mar|Abr|Mai|Jun|Jul|Ago|Set|Out|Nov|Dez)[a-z]*  # Month name
-            (\s+de\s+)?
-            """ + ordinal_pattern + """
-            (\s+de\s+)?
-            (?:                         # Year is optional
-                \s*
-                \d{4}                   # Year
-            )?
-        )
+
     """
     matches = list(re.finditer(date_pattern, doc.text, re.VERBOSE))
     new_ents = []
     for match in matches:
+        # print(match)
         start_char, end_char = match.span()
         # Convert character offsets to token offsets
         start_token = None
@@ -143,18 +139,26 @@ def find_dates(doc):
                 end_token = token.i
         if start_token is not None and end_token is not None:
             hit_text = doc.text[start_char:end_char]
-            parsed_date = dateparser.parse(hit_text)
+            # print(hit_text)
+            for k, v in reversed_ordinal_to_number.items():
+                hit_text = re.sub(k, v, hit_text)
+            hit_text = re.sub('\s*dia\s*', '', hit_text)
+            # print(hit_text)
+            
+            parsed_date = dateparser.parse(hit_text, languages=['en', 'pt'])
+            # print(parsed_date)
             if parsed_date:  # Ensure the matched string is a valid date
                 ent = Span(doc, start_token, end_token + 1, label="DATE")
                 ent._.date = parsed_date
                 new_ents.append(ent)
+                # print(ent)
             else:
                 # Replace each ordinal in hit_text with its numeric representation
                 for ordinal, number in ordinal_to_number.items():
                     hit_text = hit_text.replace(ordinal, number)
 
                 # Remove the word "of" from hit_text
-                new_date = hit_text.replace(" of ", " ")
+                new_date = hit_text.replace(" de ", " ")
 
                 parsed_date = dateparser.parse(new_date)
                 ent = Span(doc, start_token, end_token + 1, label="DATE")
@@ -164,3 +168,18 @@ def find_dates(doc):
     doc.ents = list(doc.ents) + new_ents
     
     return doc
+
+if __name__ == '__main__':
+    import spacy
+    from date_spacy import find_dates
+    nlp = spacy.blank('pt')
+    nlp.add_pipe('find_dates')
+
+    doc = nlp('''o evento está agendado pro dia 25 de Agosto de 2023.
+            Nós também temos uma reunião para 10 de setembro e uma outra no décimo segundo dia de outubro de 2024
+            décimo-segundo dia de outubro de 2024
+            e finaliza no dia 4 de Jan''')
+    # doc = nlp('''no vigésimo-segundo dia de outubro de 2024''')
+    for ent in doc.ents:
+        if ent.label_ == 'DATE':
+            print(f'Text: {ent.text} -> Parsed Date: {ent._.date}')
